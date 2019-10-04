93735231f5364fc1a316e1d18ce8043edeece93b
Eli Schwartz <eschwartz@archlinux.org>
2019-10-04 12:20:06 -0400

    Don't assume the user's makepkg.conf default BUILDDIR.

--- makepkg-source.sh
+++ makepkg-source.sh
@@ -9,7 +9,7 @@ sourcedest_path=$(readlink -f ./sourcedest)
 
 for dir in ./asp/*; do
 	cd "$startdir/$dir/trunk"
-	SRCDEST="${sourcedest_path}" makepkg --nobuild --nodeps --noprepare --skippgpcheck --skipinteg
+	SRCDEST="${sourcedest_path}" BUILDDIR=. makepkg --nobuild --nodeps --noprepare --skippgpcheck --skipinteg
 	d=$(find src/ -maxdepth 1 ! -path src/ -type d)
 	mkdir -p $source_path/$(basename $dir)
 	mv $d $source_path/$(basename $dir)
